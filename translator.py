49f403d170adc2ab8a6cfced7e07f2f690afb960
Homin Lee <homin.lge@gmail.com>
2024-06-08 11:18:57 +0900

    Fix

--- translator.py
+++ translator.py
@@ -6,7 +6,6 @@ COMMAND = "다음 영문을 설명이나 요약을 덧붙이지 말고 그대로
 SLEEP_TIME = 20
 
 def configure_genai():
-    """Configures the Gemini AI model with current settings."""
     safety = [
         {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_NONE"},
         {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_NONE"},
@@ -30,7 +29,6 @@ def show_paragraph(chat, text):
         st.write(response.text)
 
 def main():
-    # Main function to control the app flow
     st.set_page_config(page_title="Gemini Translator Eng2Kor", page_icon=":closed_book:")
     if "auth" not in st.session_state:
         form = st.form(key="auth")
@@ -39,20 +37,20 @@ def main():
         if submit:
             if passwd == st.secrets["passwd"]:
                 st.session_state.auth = "OK"
+                genai.configure(api_key=st.secrets["api_key"])
             st.rerun()
 
-    genai.configure(api_key=st.secrets["api_key"])
-    # Text input interface
-    form = st.form(key="box")
-    text = form.text_area("Input English text here")
-    submit = form.form_submit_button("Translate into Korean")
-    if submit:
-        model = configure_genai()
-        chat = model.start_chat()
-        paragraphs = text.split('\n\n')
-        for para in paragraphs:
-            show_paragraph(chat, para)
-            time.sleep(SLEEP_TIME)
+    if st.session_state.auth == "OK":
+        form = st.form(key="box")
+        text = form.text_area("Input English text here")
+        submit = form.form_submit_button("Translate into Korean")
+        if submit:
+            model = configure_genai()
+            chat = model.start_chat()
+            paragraphs = text.split('\n\n')
+            for para in paragraphs:
+                show_paragraph(chat, para)
+                time.sleep(SLEEP_TIME)
 
 if __name__ == "__main__":
     main()
